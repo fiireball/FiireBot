3144df94b879074a43f53b06015b9a7c04104cce
fiireball <adrian.fiire@gmail.com>
2021-04-19 18:24:29 +0200

    initial commit

---
+++ src/main/java/com/fiireball/maven/fiirebot/App.java
@@ -0,0 +1,13 @@
+package com.fiireball.maven.fiirebot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/fiireball/maven/fiirebot/AppTest.java
@@ -0,0 +1,14 @@
+package com.fiireball.maven.fiirebot;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
