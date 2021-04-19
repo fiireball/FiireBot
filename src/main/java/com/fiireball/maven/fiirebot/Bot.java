6a6807d4c82684c1dcac8d654c40c557e812743e
fiireball <adrian.fiire@gmail.com>
2021-04-19 21:15:17 +0200

    added logback

--- src/main/java/com/fiireball/maven/fiirebot/Bot.java
+++ src/main/java/com/fiireball/maven/fiirebot/Bot.java
@@ -1,14 +1,30 @@
 package com.fiireball.maven.fiirebot;
 
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 
+
+
+
+
 public class Bot {
 	
+
+	private static final Logger logger = LoggerFactory.getLogger(Bot.class);
+
+	
 	 public static void create(String token) {
+		 	logger.info("Starting to create {} instance", Bot.class.getSimpleName());
 		    final DiscordClient client = DiscordClient.create(token);
 		    final GatewayDiscordClient gateway = client.login().block();
 
@@ -19,7 +35,7 @@ public class Bot {
 		        channel.createMessage("Pong!").block();
 		      }
 		    });
-
+		    logger.info("Done creating {} instance", Bot.class.getSimpleName());
 		    gateway.onDisconnect().block();
 		  }
 }
--- src/test/java/com/fiireball/maven/fiirebot/AppTest.java
+++ src/test/java/com/fiireball/maven/fiirebot/AppTest.java
@@ -1,6 +1,5 @@
 package com.fiireball.maven.fiirebot;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
